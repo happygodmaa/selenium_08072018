be2eb0d03995a04cda8cc9d0eeba32cb8901de34
NIKHIL KUMAR <nkumar@wundermancommerce.com>
2021-06-17 12:21:15 +0530

    uppdated java file loginpage

--- freeCRMTest/src/main/java/com/freeCRM/Page/ContactsPage.java
+++ freeCRMTest/src/main/java/com/freeCRM/Page/ContactsPage.java
@@ -3,29 +3,26 @@ package com.freeCRM.Page;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import com.freeCRM.TestBase.TestBase;
 
 public class ContactsPage extends TestBase {
 
-		@FindBy(xpath="//option[contains(text(),415298)]")
-		WebElement Owner;
-	
-		@FindBy(xpath="//td[contains(text(), 'contacts') AND @class=datacardtitle]")
-		WebElement Contact;
-		
-
-public ContactsPage() {
-PageFactory.initElements(driver, this);	
-
-}
-
-public WebElement ValidateOwner() {
-	return Owner;
-	
-}
+  @FindBy(xpath = "//option[contains(text(),415298)]")
+  WebElement Owner;
+  /// test
+  @FindBy(xpath = "//td[contains(text(), 'contacts') AND @class=datacardtitle]")
+  WebElement Contact;
 
 
+  public ContactsPage() {
+    PageFactory.initElements(driver, this);
+
+  }
+
+  public WebElement ValidateOwner() {
+    return Owner;
+
+  }
 
 
 
